c722a3c17d2b849c39cd159446eaff17224fd0f8
Irina Kamneva <brynza80@gmail.com>
2025-06-13 13:13:24 +0300

    Initial

--- partners/permissions.py
+++ partners/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class IsActiveUser(permissions.BasePermission):
     """Разрешает доступ только активным пользователям"""
 
-    # def has_permission(self, request, view):
-    #     return bool(request.user and request.user.is_active)
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_active)
